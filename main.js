dc1a9bafa5258b3aa9d38b45c7b09d6b77756d61
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-06-17 17:21:29 -0700

    new functions were added

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css';
 import getRandomWord from './src/randomWord.js';
 import setSharkImage from './src/sharkImage.js';
-import setupWord from './src/word.js';
-import isLetterInWord from './src/word.js';
+import { setupWord, isLetterInWord, revealLetterInWord } from './src/word.js';
+import setupGuesses from './src/guess.js';
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -21,6 +21,7 @@ const initSharkwords = () => {
 
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
+
   setSharkImage(document.querySelector('#shark-img'), numWrong);
 
   setupWord(document.querySelector('#word-container'), word);
@@ -28,6 +29,28 @@ const initSharkwords = () => {
   console.log(isLetterInWord(word[0]), '(should be true)');
   console.log(isLetterInWord('1'), '(should be false)');
 
-};
+  revealLetterInWord(word[0]);
+  
+  // sharkImgEl = document.querySelector('#shark-img')'
+  const handleGuess = (guessEvent, letter) => {
+  console.log(`guessEvent is: ${guessEvent}`);
+  console.log(`letter is: ${letter}`);
+
+  // Disable button after click
+  const button = guessEvent.target;
+    button.setAttribute('disabled', true);
+
+  //Handle correct/incorrect guess
+    if (isLetterInWord(letter)) {
+     revealLetterInWord(letter);
+    } else {
+      numWrong += 1;
+      setSharkImage(sharkImgEl, numWrong);
+    }
+  };
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess);
+
+  
+  }
 
 initSharkwords();
--- src/word.js
+++ src/word.js
@@ -1,6 +1,6 @@
 let word;
 
-function setupWord(element, initWord) {
+export function setupWord(element, initWord) {
     word = initWord;
 
     word.split('').forEach(() => {
@@ -8,12 +8,15 @@ function setupWord(element, initWord) {
     });
 }
 
-function isLetterInWord(letter) {
-return word.includes(letter);
+export function isLetterInWord(letter) {
+    return word.includes(letter);
 }
 
-function revealLetterInWord(letter) {}
-
-export default setupWord(); isLetterInWord(); revealLetterInWord();
-// export default isLetterInWord;
-// export default revealLetterInWord;
+export function revealLetterInWord(letter) {
+    const letterBoxes = document.querySelectorAll('.letter-box');
+    word.split('').forEach((wordLetter, idx) => {
+        if (wordLetter === letter) {
+            letterBoxes[idx].innerHTML = letter;
+        }
+    });
+}
